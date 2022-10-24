6cb76a8e4be748f3622be15a73931b3b492a82d5
sdehury73 <104459049+sdehury73@users.noreply.github.com>
2022-10-24 09:18:38 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -4,9 +4,9 @@ public class Program1 {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the first number(a)");
-		double a=s.nextInt();
+		double a=s.nextDouble();
 		System.out.println("Enter the second number(b)");
-		double b=s.nextInt();
+		double b=s.nextDouble();
 		System.out.println("Enter the type of operation");
 		String operation=s.next();
 		switch(operation)
